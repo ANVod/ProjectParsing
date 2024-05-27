06d0acc77e78e133f7f9c44de5c8d19de8519b24
ANVod <157865931+ANVod@users.noreply.github.com>
2024-05-27 20:14:51 +0300

    add book

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from bs4 import BeautifulSoup
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts"
-params = {
-    "userId" : 1
-}
-response = requests.get(url, params=params)
-if response.status_code == 200:
-    posts = response.json()
-    for post in posts:
-        print(post)
-else:
-    print("Error")
+url = "http://quotes.toscrape.com/"
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+text = soup.find_all("span", class_="text")
+
+author = soup.find_all("small", class_="author")
+print(text)
+for i in range(len(text)):
+    print(f"Цитата номер -{i + 1}")
+    print(text[i].text)
+    print(f"Автор цитаты - {author[i].text}\n")
+
