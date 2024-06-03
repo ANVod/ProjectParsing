44dd5e10893ca300be97147829e3ed7cf57341ac
ANVod <157865931+ANVod@users.noreply.github.com>
2024-06-03 23:48:45 +0300

    add corrent code

--- leroymerlin_mirrors_spider.py
+++ leroymerlin_mirrors_spider.py
@@ -2,21 +2,22 @@
 import scrapy
 
 class LeroymerlinMirrorsSpider(scrapy.Spider):
-    name = "leroymerlin_mirrors"
-    allowed_domains = ["leroymerlin.ru"]
-    start_urls = ["https://leroymerlin.ru/catalogue/zerkala-v-vannuyu-komnatu/"]
+    name = "leroymerlin_mirrors" # название
+    allowed_domains = ["leroymerlin.ru"] # домен
+    start_urls = ["https://leroymerlin.ru/catalogue/zerkala-v-vannuyu-komnatu/"] # начальная страница
 
-    def parse(self, response):
-        mirrors = response.css('div.pr7cfcb_plp largeCard')
+    def parse(self, response): # функция для парсинга
+        # Обновленные селекторы для зеркал
+        mirrors = response.css('div.f19v7pn1_plp.largeCard') # селектор для зеркал
 
-        for mirror in mirrors:
+        for mirror in mirrors: # для каждого зеркала
             yield {
-                'name': mirror.css('div.p1h8lbu4_plp span::text').get(),
-                'price': mirror.css('div.mncjxni_plp span::text').get(),
-                'url': mirror.css('a.bex6mjh_plp').attrib['href']
+                'name': mirror.css('span[data-qa="product-name"]::text').get(), # селектор для названия
+                'price': mirror.css('span[data-qa="product-price"]::text').get(), # селектор для цены
+                'url': response.urljoin(mirror.css('a[data-qa="product-link"]').attrib['href']) # селектор для ссылки
             }
 
         # Переход на следующую страницу, если она есть
-        next_page = response.css('a[data-qa-pagination-item="right"]::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, self.parse)
+        next_page = response.css('a[data-qa-pagination-item="right"]::attr(href)').get() #
+        if next_page: # если есть
+            yield response.follow(next_page, self.parse) # переход
