705e59ca9774188be6e7e010fffb7c8caa06724a
ANVod <157865931+ANVod@users.noreply.github.com>
2024-06-06 22:28:57 +0300

    add PS01 #

--- PS01.py
+++ PS01.py
@@ -1,19 +1,18 @@
 #PS01 - Парсинг HTML-данных с помощью BeautifulSoup
-
 from bs4 import BeautifulSoup
 import requests
 
 url = "http://quotes.toscrape.com/"
-response = requests.get(url)
-html = response.text
+response = requests.get(url) # Запрос
+html = response.text # Текст
 
-soup = BeautifulSoup(html, "html.parser")
-text = soup.find_all("span", class_="text")
+soup = BeautifulSoup(html, "html.parser") # Парсинг
+text = soup.find_all("span", class_="text") # Поиск
 
-author = soup.find_all("small", class_="author")
-print(text)
-for i in range(len(text)):
-    print(f"Цитата номер -{i + 1}")
-    print(text[i].text)
-    print(f"Автор цитаты - {author[i].text}\n")
+author = soup.find_all("small", class_="author") # Поиск
+print(text) # Вывод
+for i in range(len(text)): # Цикл
+    print(f"Цитата номер -{i + 1}") # Вывод
+    print(text[i].text) # Вывод
+    print(f"Автор цитаты - {author[i].text}\n") # Вывод
 
